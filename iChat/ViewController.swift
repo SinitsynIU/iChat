049d7478a567a3cd73b8d2fd15bf472977d62bdd
Ilya Sinitsyn <Best45@inbox.ru>
2022-07-04 21:54:06 +0500

    no message

---
+++ iChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iChat
+//
+//  Created by Илья Синицын on 04.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
